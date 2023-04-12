37c2c21224c7210c209ffab8e90311f2bc953a59
silomalo <silomalojoseph@gmail.com>
2023-04-12 12:53:17 +0300

    not yet

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,39 +1,45 @@
 import { useState } from "react";
-
+// import { csrfToken } from "next-auth/client";
 
 export default function User() {
-const [data, setData] = useState<any[]>([]);
-const [formData, setFormData] = useState({
-  name: "",
-  content: "",
-});
+  const [data, setData] = useState<any[]>([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    dob: "",
+    member_type: "",
+    nomination_year: "",
+  });
 
-const handleSubmit = async (e: any) => {
-  e.preventDefault();
-  try {
-    console.log(formData);
-    // const res = await fetch(`${process.env.REACT_APP_API_URL}/posts`, {
-    const res = await fetch(`http://localhost:8000/api/posts`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    if (res.ok) {
-      const newData = await res.json();
-      setData((data) => [...data, newData]);
-      console.log(data);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      // const res = await fetch(`${process.env.REACT_APP_API_URL}/users`, {
+      const res = await fetch(`http://localhost:8000/api/addUser`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+
+      });
+      console.log(res);
+      if (res.ok) {
+        const newData = await res.json();
+        setData((data) => [...data, newData]);
+        console.log(data);
+      }
+      // setFormData({
+      //   name: "",
+      //   email: "",
+      //   dob: "",
+      //   member_type: "",
+      //   nomination_year: "",
+      // });
+    } catch (error) {
+      console.log(error);
     }
-    setFormData({
-      name: "",
-      content: "",
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
+  };
 
   return (
     <form
@@ -46,7 +52,6 @@ const handleSubmit = async (e: any) => {
         </label>
         <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="name"
           type="text"
           name="name"
           placeholder="Enter your name"
@@ -56,17 +61,56 @@ const handleSubmit = async (e: any) => {
       </div>
       <div className="mb-4">
         <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
-          Name
+          Email
         </label>
-        <textarea
+        <input
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="content"
-          name="content"
-          rows={5}
-          placeholder="Enter your content"
-          value={formData?.content}
+          type="email"
+          name="name"
+          placeholder="Enter your email"
+          value={formData?.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+          Date of birth
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="date"
+          name="member_type"
+          value={formData?.dob}
+          onChange={(e) => setFormData({ ...formData, dob: e.target.value })}
+        />
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+          Member type
+        </label>
+        <select
+          className="block  w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          value={formData?.member_type}
           onChange={(e) =>
-            setFormData({ ...formData, content: e.target.value })
+            setFormData({ ...formData, member_type: e.target.value })
+          }
+        >
+          <option>Option 1</option>
+          <option>Option 2</option>
+          <option>Option 3</option>
+        </select>
+      </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-bold mb-2" htmlFor="name">
+          Nomination year
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="date"
+          name="nomination_year"
+          value={formData?.nomination_year}
+          onChange={(e) =>
+            setFormData({ ...formData, nomination_year: e.target.value })
           }
         />
       </div>
